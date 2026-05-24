165804989c5b448aed2eefdc5edb554c9dcba3f4
Sun Geer <1013296326@qq.com>
2026-05-25 07:35:24 +0800

    Update jobs.py

--- src/domains/health/jobs.py
+++ src/domains/health/jobs.py
@@ -1,8 +1,25 @@
+from loguru import logger
+
 from src.domains.health import repository
 from src.core.db_registry import db
 
+logger.add(
+      'logs/cron/sync_users.log',
+      rotation='50 MB',
+      retention=3,
+      format='{time:YYYY-MM-DD HH:mm:ss} - {level} - [{extra[run_id]}] {name}:{function}:{line} -
+  {message}',
+      encoding='utf-8',
+      enqueue=True,
+      level='INFO',
+      filter=lambda r: r['extra'].get('task') == 'sync_users',
+  )
+
+  log = logger.bind(task='sync_users')
+
 
 def check_db_conn():
+    log.info('开始同步用户数据...')
     with db.connect() as cursor:
         repository.check_db_conn(cursor)
 
